143542d748645af62bccd36625e0e0a6dc4cb8a5
constant01 <90539154+constant01@users.noreply.github.com>
2022-08-03 17:34:14 -0600

    Initial commit

---
+++ js/myCode.js
@@ -0,0 +1,16 @@
+function animarSVGDraw() {
+    /*
+    var tween1 = KUTE.fromTo('#letraT',{draw:'0% 0%'}, {draw:'0% 100%'},{duration: 1500, easing: "easingCubicIn", offset: 250}).start();
+    var tween2 = KUTE.fromTo('#letraR',{draw:'0% 10%'}, {draw:'0% 100%'}, {duration: 2500, easing: "easingCubicOut", offset: 250}).start();
+    var tween3 = KUTE.fromTo('#letraE',{draw:'90% 105%'}, {draw:'0% 105%'}, {duration: 1500, easing: "easingCubicIn", offset: 250}).start();
+    var tween4 = KUTE.fromTo('#letraS',{draw:'0% 15%'}, {draw:'0% 100%'}).start();
+    var tween5 = KUTE.fromTo('#letraRR',{draw:'0% 10%'}, {draw:'0% 100%'}).start();
+    var tween6 = KUTE.fromTo('#letraI',{draw:'0% 10%'}, {draw:'0% 100%'}).start();
+    var tween7 = KUTE.fromTo('#letraO',{draw:'0% 5%'}, {draw:'0% 100%'}).start();
+    var tween8 = KUTE.fromTo('#letraSS',{draw:'0% 5%'}, {draw:'0% 100%'}).start();
+    var tween9 = KUTE.fromTo('#mano',{draw:'0% 3%'}, {draw:'90% 100%'},{duration: 7000, easing: "easingExponentialInOut", offset: 250}).start();
+    */
+
+    //Variables de "ranaDraw"
+    var tween1 = KUTE.fromTo('#ranaDraw',{draw:'0% 0%'}, {draw:'0% 100%'},{duration: 1500, easing: "easingCubicIn", offset: 250}).start();
+}
